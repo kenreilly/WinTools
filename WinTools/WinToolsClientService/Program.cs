da372900089f0f0adc4052e9aea688bf22814d6b
kenreilly <kenneth.reilly@hotmail.com>
2015-01-17 17:49:31 -0600

    Project structure

---
+++ WinTools/WinToolsClientService/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+using System.Management.Instrumentation;
+
+namespace WinToolsClientService
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		static void Main()
+		{
+			ServiceBase[] ServicesToRun;
+			ServicesToRun = new ServiceBase[] 
+            { 
+                new Service1() 
+            };
+			ServiceBase.Run(ServicesToRun);
+		}
+
+
+		public static string getAssetTag()
+		{
+			string cpuInfo = string.Empty;
+			ManagementClass mc = new ManagementClass("win32_processor");
+			ManagementObjectCollection moc = mc.GetInstances();
+
+			foreach (ManagementObject mo in moc)
+			{
+				cpuInfo = mo.Properties["processorID"].Value.ToString();
+				break;
+			}
+			return string.Empty;
+		}
+	}
+}
---
+++ WinTools/WinToolsDB/Machines.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Machines]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [AssetTag] VARCHAR(50) NULL
+)
